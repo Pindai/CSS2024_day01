0bf6d5fd70a80ba9d5b0a455e9baabab3ca1279c
Pindai <157530796+Pindai@users.noreply.github.com>
2024-01-30 11:01:38 +0200

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 14:41:42 2024
+
+@author: Pindai
+"""
+
+"""
+Storing data in Python
+
+1. Lists
+2. Dictionaries
+3. Data Frames - specific to pandas
+"""
+# Country data
+import pandas
+file = pandas.read_csv("country_data.csv")
+print(file)
+"""
+ Age Gender       Country
+0    39      M  South Africa
+1    25      M      Botswana
+2    29      F  South Africa
+3    46      M  South Africa
+4    22      F         Kenya
+5    35      F    Mozambique
+6    22      F       Lesotho
+7    49      M         Kenya
+8    30      M         Kenya
+9    40      F         Egypt
+10   30      M         Sudan
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
